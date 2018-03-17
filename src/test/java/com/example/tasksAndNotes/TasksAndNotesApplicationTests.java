c7e2be31d9ffa6eb69ef21ee5c8198ea95c289bb
Adam Krygier <adamk7766@gmail.com>
2018-03-17 11:28:52 +0100

    first commit

---
+++ src/test/java/com/example/tasksAndNotes/TasksAndNotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tasksAndNotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TasksAndNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
